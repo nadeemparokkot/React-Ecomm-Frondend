d50c5eb4cbb73bd9a1713cef575a0ba30f47194f
Nadeem Parokkot <nadeemparokkot@gmail.com>
2024-08-05 23:10:56 +0530

    created a basic ecomm

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Home from './Components/Home';
+import Login from './Components/Login';
+import Register from './Components/Register';
+import Navbar from './Components/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Register />} />
+        </Routes>
+    
+
+    </div>
+  );
+}
+
+export default App;
